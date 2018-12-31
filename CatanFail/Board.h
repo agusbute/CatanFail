535b86e6f601c5fdbe38829f1d08073969f18750
Pilar <pilii.cres@gmail.com>
2018-12-31 03:25:28 -0300

    tenemos dados en el tablero

--- CatanFail/Board.h
+++ CatanFail/Board.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <time.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 class Board
 {
-private:
 public:
+	int throw_the_dices(void);	// "tira los dados" y devuelve la suma de ambos
+private:
 
 };
