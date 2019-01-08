14c2d1ad1364331d3dabb6561effa106609c8236
Pilar <pilii.cres@gmail.com>
2019-01-08 16:42:22 -0300

    un poquito más en player, estoy sufriendo

--- CatanFail/Player.cpp
+++ CatanFail/Player.cpp
@@ -1,18 +1,102 @@
 #include "Player.h"
 
 Player::
-Player(string name_, int v_points, Player* player)
+Player(string name, int v_points)
 {
-	setName(name_, player);
+	setName(name);
+	setVictoryPoints(v_points, EQUAL);
 }
 
 void Player::
-setName(string name, Player* player)
+setName(string name)
 {
-	player->name = name;
+	this->name = name;
 }
 
+int Player::
+getVictoryPoints(void)
+{
+	return this->victory_points;
+}
 
+bool Player::
+setVictoryPoints(int v_points, Actions action)
+{
+	switch (action)
+	{
+		case ADD:
+			this->victory_points += v_points;
+			break;
+
+		case SUBTRACT:
+			this->victory_points -= v_points;	// ¿puedo tener victory points negativos?
+			break;
+
+		case EQUAL:
+			this->victory_points = v_points;
+			break;
+
+		default: 
+			return 1;
+			break;
+	}
+
+	return 0;
+}
+
+int Player:: 
+getRoadsBuilt(void)
+{
+	return this->roads_built;
+}
+
+int Player::
+getSettlementsBuilt(void)
+{
+	return this->settlements_built;
+}
+
+int Player::
+getCitiesBuilt(void)
+{
+	return this->cities_built;
+}
+
+// y para esto me faltan hacer las listas ugh 
+int Player::
+getDevelopmentCard( DevelopmentCards d_card)
+{
+
+}
+
+int Player::
+getResourceCard(ResourceCards r_card)
+{
+
+}
+
+// todas estas necesitan los métodos de validación bool canBuild...Here( coords )
+bool buildRoad();
+bool buildSettlement();
+bool buildCity();
+
+// tambien las validaciones
+void moveRobber();
+void domesticTrade(ResourceCards my_r_card, ResourceCards the_r_card_i_want);
+void maritimeTrade();
+
+bool Player::
+GameWon(void)
+{
+	if (getVictoryPoints() >= 8)
+	{
+		return 0;	// i won
+	}
+	else
+	{
+		return 1; // not yet
+	}
+}
 
 void Player::
 throw_the_dices(void)
@@ -23,4 +107,6 @@ throw_the_dices(void)
 	second_dice = rand() % 6 + 1;	//tiro el segundo dado
 	dice_number = first_dice + second_dice;
 }
+
+//~Player();
 
--- CatanFail/Player.h
+++ CatanFail/Player.h
@@ -1,35 +1,44 @@
 #pragma once
 
 #include <iostream>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
 #include <time.h>
+
+using namespace :: std;
+
+enum Actions { ADD, SUBTRACT, EQUAL};
+enum DevelopmentCards {K, V, C, M, P};	// Development Cards: Knight, Victory Point, ..., ..., Progress
+enum ResourceCards {M, L, P, T, O, N}; //Resource Cards: Lumber, Brick, Ore, Grain, Wood, Nothing
 
 class Player
 {
 	public:
-
 		Player(string name, int v_points);
 		void setName(string name);
 		int getVictoryPoints(void);
-		void setVictoryPoints(int v_points);
+		bool setVictoryPoints(int v_points, Actions action);
 		int getRoadsBuilt(void);
 		int getSettlementsBuilt(void);
 		int getCitiesBuilt(void);
-		int getDevelopmentCard(void);
-		int getResourceCard(char r_card); 	//se fija si tiene la carta de recurso que se requiere
+		int getDevelopmentCard(DevelopmentCards d_card);	//estas son opcionales
+		int getResourceCard(ResourceCards r_card); 	//se fija si tiene la carta de recurso que se requiere
 
 		bool buildRoad();
 		bool buildSettlement();
 		bool buildCity();
 
 		void moveRobber();
-		void tradeResourceCard(char r_card);
+		void domesticTrade(ResourceCards r_card, ResourceCards the_r_card_i_want); // with opponent // puedo hacer trades de mas de una carta!!!!!
+		void maritimeTrade(); 
+
 
 		void throw_the_dices(void);
 
-		~Player();
+		bool GameWon(); // returns 0 if i won
+
+		//~Player();
 
 	private:
 		string name;
@@ -38,6 +47,6 @@ class Player
 		int settlements_built;
 		int cities_built;
 		//char* resource_cards;   \ deberían ser
-		//char* developmen_cards; /    listas
+		//char* developmen_cards; /    listas		//estas son opcionales
 		int dice_number;
 };
