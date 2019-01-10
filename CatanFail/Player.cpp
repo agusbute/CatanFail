8c1d19123e214fc1255c749b30fcf55160fee5fe
Pilar <pilii.cres@gmail.com>
2019-01-09 21:59:57 -0300

    casi casi que está player (mentira falta banda seguro)

--- CatanFail/Player.cpp
+++ CatanFail/Player.cpp
@@ -1,10 +1,23 @@
 #include "Player.h"
 
 Player::
-Player(string name, int v_points)
+Player(string name)
 {
 	setName(name);
-	setVictoryPoints(v_points, EQUAL);
+
+	// setting all resource cards = 0
+	r_cards.brick = 0;
+	r_cards.grain = 0;
+	r_cards.lumber = 0;
+	r_cards.ore = 0;
+	r_cards.wool = 0;
+
+	// setting all development cards = 0
+	d_cards.knight = 0;
+	d_cards.largest_army = 0;
+	d_cards.longest_road = 0;
+	d_cards.progress = 0;
+	d_cards.victory_point = 0;
 }
 
 void Player::
@@ -13,14 +26,14 @@ setName(string name)
 	this->name = name;
 }
 
-int Player::
+unsigned int Player::
 getVictoryPoints(void)
 {
 	return this->victory_points;
 }
 
 bool Player::
-setVictoryPoints(int v_points, Actions action)
+setVictoryPoints(unsigned int v_points, Actions action)
 {
 	switch (action)
 	{
@@ -29,83 +42,229 @@ setVictoryPoints(int v_points, Actions action)
 			break;
 
 		case SUBTRACT:
-			this->victory_points -= v_points;	// ¿puedo tener victory points negativos?
-			break;
-
-		case EQUAL:
-			this->victory_points = v_points;
+			if (this->victory_points > 0)
+			{
+				this->victory_points -= v_points; // supongo que no puedo tener victory points negativos
+			}
+			else
+			{
+				this->error = NEG_VP;
+				return 1;
+			}
 			break;
 
 		default: 
-			return 1;
+			return 1;	
 			break;
 	}
 
 	return 0;
 }
 
-int Player:: 
+unsigned int Player::
 getRoadsBuilt(void)
 {
 	return this->roads_built;
 }
 
-int Player::
+unsigned int Player::
 getSettlementsBuilt(void)
 {
 	return this->settlements_built;
 }
 
-int Player::
+unsigned int Player::
 getCitiesBuilt(void)
 {
 	return this->cities_built;
 }
 
-// y para esto me faltan hacer las listas ugh 
-int Player::
+bool Player::
 getDevelopmentCard( DevelopmentCards d_card)
 {
+	switch (d_card)
+	{
+		case KNIGHT:
+			if (d_cards.knight >= 1)
+			{
+				d_cards.knight -= 1;
+			}
+			else
+			{
+				error = NO_DCARD;
+				return 0;
+			}
+			break;
+		
+		case V_POINT:
+			if (d_cards.victory_point >= 1)
+			{
+				d_cards.victory_point -= 1;
+			}
+			else
+			{
+				error = NO_DCARD;
+				return 0;
+			}
+			break;
 
+		case L_ROAD:
+			if (d_cards.longest_road >= 1)
+			{
+				d_cards.longest_road-= 1;
+			}
+			else
+			{
+				error = NO_DCARD;
+				return 0;
+			}
+			break;
+
+		case L_ARMY:
+			if (d_cards.largest_army >= 1)
+			{
+				d_cards.largest_army -= 1;
+			}
+			else
+			{
+				error = NO_DCARD;
+				return 0;
+			}
+			break;
+
+		case PROG:
+			if (d_cards.progress >= 1)
+			{
+				d_cards.progress -= 1;
+			}
+			else
+			{
+				error = NO_DCARD;
+				return 0;
+			}
+			break;
+
+		default:
+			return 0;
+			break;
+	}
+
+	return 1;
 }
 
-int Player::
+/*
+unsigned int Player::
 getResourceCard(ResourceCards r_card)
 {
 
 }
+*/
 
-// todas estas necesitan los métodos de validación bool canBuild...Here( coords )
-bool buildRoad();
-bool buildSettlement();
-bool buildCity();
+bool Player::
+buildRoad(void)
+{
+	if (roads_built < 2)	//se fija si está en el primer o segundo turno
+	{
+		roads_built += 1;
+	}
+	else if (r_cards.brick >= 1 && r_cards.lumber >= 1)	//si está más avanzado el juego, se fija que tenga las cartas necesarias
+	{
+		r_cards.brick -= 1;
+		r_cards.lumber -= 1;
+		roads_built += 1;
+	}
+	else
+	{
+		return 0;	//no puede construir 
+	}
 
-// tambien las validaciones
+	return 1;
+}
+
+bool Player::
+buildSettlement(void)
+{
+	if (settlements_built < 2)	//se fija si está en el primer o segundo turno
+	{
+		settlements_built += 1;
+	}
+	else if (r_cards.brick >= 1 && r_cards.grain >= 1 && r_cards.lumber >= 1 && r_cards.wool >= 1)	//si está más avanzado el juego, se fija que tenga las cartas necesarias
+	{
+		r_cards.brick -= 1;
+		r_cards.grain -= 1;
+		r_cards.lumber -= 1;
+		r_cards.wool -= 1;
+		settlements_built += 1;
+	}
+	else
+	{
+		return 0;	//no puede construir
+	}
+
+	return 1;
+}
+
+bool Player::
+buildCity(void)
+{
+	if (settlements_built >= 1 && r_cards.ore >= 3 && r_cards.grain >= 2)	//se fija si hay al menos un settlement y si tiene las cartas de recurso necesarias
+	{
+		r_cards.ore -= 3;
+		r_cards.grain -= 2;
+		settlements_built -= 1;
+		cities_built += 1;
+	}
+	else
+	{
+		return 0;	//no puede construir
+	}
+
+	return 1;
+}
+
+/****************************************** FALTAN ESTOS **********************************************/
 void moveRobber();
 void domesticTrade(ResourceCards my_r_card, ResourceCards the_r_card_i_want);
 void maritimeTrade();
+
+bool Player::
+buyDevelopmentCard(void)
+{
+	if (r_cards.ore >= 1 && r_cards.wool >= 1 && r_cards.grain >= 1)	//se fija si tengo las cartas de recurso necesarias para comprar una development card
+	{
+		r_cards.ore -= 1;
+		r_cards.wool -= 1;
+		r_cards.grain -= 1;
+	}
+	else
+	{
+		return 0;	//no puede comprar
+	}
+
+	return 1;
+}
+
+void Player::
+throw_the_dices(void)
+{
+	unsigned int first_dice = 0, second_dice = 0;
+	srand(time(NULL));	//inicializo la semilla
+	first_dice = rand() % 6 + 1;	//tiro el primer dado
+	second_dice = rand() % 6 + 1;	//tiro el segundo dado
+	dice_number = first_dice + second_dice;
+}
 
 bool Player::
 GameWon(void)
 {
 	if (getVictoryPoints() >= 8)
 	{
-		return 0;	// i won
+		return 1;	// i won
 	}
 	else
 	{
-		return 1; // not yet
+		return 0; // not yet
 	}
-}
-
-void Player::
-throw_the_dices(void)
-{
-	int first_dice = 0, second_dice = 0;
-	srand(time(NULL));	//inicializo la semilla
-	first_dice = rand() % 6 + 1;	//tiro el primer dado
-	second_dice = rand() % 6 + 1;	//tiro el segundo dado
-	dice_number = first_dice + second_dice;
 }
 
 //~Player();
--- CatanFail/Player.h
+++ CatanFail/Player.h
@@ -8,45 +8,74 @@
 
 using namespace :: std;
 
-enum Actions { ADD, SUBTRACT, EQUAL};
-enum DevelopmentCards {K, V, C, M, P};	// Development Cards: Knight, Victory Point, ..., ..., Progress
-enum ResourceCards {M, L, P, T, O, N}; //Resource Cards: Lumber, Brick, Ore, Grain, Wood, Nothing
+enum Actions { ADD, SUBTRACT};
+enum DevelopmentCards {KNIGHT, V_POINT, L_ROAD, L_ARMY, PROG};	// Development Cards: Knight, Victory Point, Longest Road, Largest Army, Progress
+enum ResourceCards {LUMBER, BRICK, ORE, GRAIN, WOOL}; //Resource Cards: Lumber, Brick, Ore, Grain, Wool
+enum Player_Error { NEG_VP, NO_DCARD};	// Victory Points Negativos
+
+typedef struct 
+{
+	unsigned int lumber;
+	unsigned int brick;
+	unsigned int ore;
+	unsigned int grain;
+	unsigned int wool;
+} myResourseCards_t;
+
+
+typedef struct
+{
+	unsigned int knight;
+	unsigned int victory_point;
+	unsigned int longest_road;	//no se si van acá esta y la siguiente pero en la consigna están así
+	unsigned int largest_army;
+	unsigned int progress;
+} myDevelopmentCards_t;
 
 class Player
 {
-	public:
-		Player(string name, int v_points);
-		void setName(string name);
-		int getVictoryPoints(void);
-		bool setVictoryPoints(int v_points, Actions action);
-		int getRoadsBuilt(void);
-		int getSettlementsBuilt(void);
-		int getCitiesBuilt(void);
-		int getDevelopmentCard(DevelopmentCards d_card);	//estas son opcionales
-		int getResourceCard(ResourceCards r_card); 	//se fija si tiene la carta de recurso que se requiere
+public:
+	Player(string name);
+	void setName(string name);
+	unsigned int getVictoryPoints(void);
+	bool setVictoryPoints(unsigned int v_points, Actions action);
+	unsigned int getRoadsBuilt(void);
+	unsigned int getSettlementsBuilt(void);
+	unsigned int getCitiesBuilt(void);
+	bool getDevelopmentCard(DevelopmentCards d_card);	//estas son opcionales	//devuelve 0 si hubo error
+	// unsigned int getResourceCard(ResourceCards r_card);
 
-		bool buildRoad();
-		bool buildSettlement();
-		bool buildCity();
+	// devuelven uno si tiene las cartas de recurso necesarias para construir cada cosa
+	bool buildRoad(void); 
+	bool buildSettlement(void);
+	bool buildCity(void);
 
-		void moveRobber();
-		void domesticTrade(ResourceCards r_card, ResourceCards the_r_card_i_want); // with opponent // puedo hacer trades de mas de una carta!!!!!
-		void maritimeTrade(); 
+	void moveRobber();
+	void domesticTrade(ResourceCards r_card, ResourceCards the_r_card_i_want); // with opponent // puedo hacer trades de mas de una carta!!!!!
+	void maritimeTrade();
+	
+	bool buyDevelopmentCard(void);	//solamente se fija si tengo las cartas de desarrollo necesarias
 
 
-		void throw_the_dices(void);
+	void throw_the_dices(void);
 
-		bool GameWon(); // returns 0 if i won
+	bool GameWon(void); // returns 1 if i won
 
-		//~Player();
+	//~Player();
 
-	private:
-		string name;
-		int victory_points;
-		int roads_built;
-		int settlements_built;
-		int cities_built;
+private:
+	string name;
+	unsigned int victory_points;
+	unsigned int roads_built;
+	unsigned int settlements_built;
+	unsigned int cities_built;
+	myResourseCards_t r_cards;
+	myDevelopmentCards_t d_cards;
+	unsigned int dice_number;
+
+	Player_Error error;
+
+		/********************************/
 		//char* resource_cards;   \ deberían ser
 		//char* developmen_cards; /    listas		//estas son opcionales
-		int dice_number;
 };
