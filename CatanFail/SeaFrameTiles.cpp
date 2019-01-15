9411530b024788846c8e0473569704289beafb79
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2019-01-15 14:33:21 -0300

    Update a SeaFrameTiles

--- CatanFail/SeaFrameTiles.cpp
+++ CatanFail/SeaFrameTiles.cpp
@@ -7,3 +7,48 @@ SeaFrameTiles(char position_in_map) : BoardComponent(position_in_map)
 {
 	//no se como poner los puertosssssssssss
 };
+
+bool SeaFrameTiles::
+setPorts(char ports)		//devuelve true si no hubo errores
+{
+	bool ret = true; //asumo que el parametro ports es valido
+	
+	switch (ports) 
+	{
+		case 'N': case 'n':
+		{
+			myPorts.port1 = ANY;
+		}break;
+		
+		case 'T': case 't':
+		{
+			myPorts.port1 = ANY;
+			myPorts.port2 = WHEAT;
+		}break;
+
+		case 'O': case 'o':
+		{
+			myPorts.port1 = ANY;
+			myPorts.port2 = SHEEP;
+		}break;
+			
+		case 'L': case 'l':
+		{
+			myPorts.port1 = ANY;
+			myPorts.port2 = BRICK;
+		}break;
+		
+		case 'P': case 'p':
+		{
+			myPorts.port1 = STONE;
+		}break;
+		
+		case 'M': case 'm':
+		{
+			myPorts.port1 = WOOD;
+		}break;
+
+		default: ret = false; break;  //se introdujo un caracter incorrectos
+	}
+	return ret;
+}
--- CatanFail/SeaFrameTiles.h
+++ CatanFail/SeaFrameTiles.h
@@ -9,12 +9,20 @@
 
 using namespace :: std;
 
+typedef enum PORT {NONE, SHEEP, WOOD, STONE, BRICK, WHEAT, ANY};
+
+typedef struct {
+	PORT port1;
+	PORT port2 = NONE;
+}ports_t;
+
 class SeaFrameTiles : public BoardComponent
 {
 	public:
-		SeaFrameTiles(char position_in_map = 0);
-
+		SeaFrameTiles(char position_in_map = 0); 
+		bool setPorts(char ports); //char ports son que puertos tiene (identificado por 'N', 'T', 'O', 'L', 'P', 'M')
 
 	private:
-		int harbor; //???????
+		ports_t myPorts;
+		char adjacentPieces[5] = {0,0,0,0,0}; //Todos los Sea Frame Tiles tienen 5 piezas adyacentes(2 Sea frames, 3 hexes)
 };
