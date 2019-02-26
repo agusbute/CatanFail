fd27e7a98b4ca60b019c088471efae21186d0f8b
Defalt <Defalt@DEFALT-PC>
2019-02-26 17:48:32 -0300

    We are back ma bois

---
+++ CatanFail/SocketControl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "boost/asio.hpp"
+#include <string>
+#include <deque>
+
+using namespace std;
+
+#define BUFFER_SIZE	1024
+
+class NetworkSocket {
+	public:
+
+		enum Modo : int { CLIENT, SERVER }; //modos a funcionar
+
+	
+};
